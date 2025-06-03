f340db23824afe6a5c87ed639687343d6906fefb
Nazar <nazar.zhuvaka.mcboss@gmail.com>
2025-06-03 23:09:47 +0200

    Add some func

---
+++ TarasPisun.js
@@ -0,0 +1,18 @@
+const root = document.querySelector("#root");
+const btn = createElement("button", { classNames: ["list-day"] }, "Click me!");
+
+root.append(btn)
+
+btn.addEventListener('click', myFunc)
+
+function myFunc() {
+    alert('Taras chmo ebanoe')
+}
+
+function createElement(type, { classNames }, ...childNodes) {
+  const elem = document.createElement(type);
+  elem.classList.add(...classNames);
+  elem.append(...childNodes);
+
+  return elem;
+}
